167add3f5a4fe4e9913d17afc9db38827cb516ed
Anastasya Ropot <a.ropot@syberry.com>
2020-03-28 15:53:34 +0300

    5: Create merge sort

---
+++ mergeSort.js
@@ -0,0 +1,27 @@
+function merge(left, right) {
+    let result = [];
+
+    while (left.length > 0 && right.length > 0) {
+        if (left[0] < right[0]) {
+            result.push(left.shift());
+        } else {
+            result.push(right.shift());
+        }
+    }
+
+    return [...result, ...left, ...right];
+}
+
+function mergeSort(arr) {
+    if (arr.length < 2) {
+        return arr;
+    }
+
+    const middle = Math.floor(arr.length / 2);
+    const left = arr.slice(0, middle);
+    const right = arr.slice(middle);
+
+    return merge(mergeSort(left), mergeSort(right));
+}
+
+console.log(mergeSort([1,6,3,2,5,9]));
